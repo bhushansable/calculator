11b8c50a8ee309365dc1c221536daa311cd79d2c
bhushansable <bhushansable9876@gmail.com>
2026-01-15 04:08:21 +0530

    add keybord function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
 
 const display = document.getElementById("display");
 
-
 function appendToDisplay(input){
     display.value += input;
 }
@@ -17,3 +16,35 @@ function claculate(){
     display.value ="Errror"
   }
 }
+
+
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  // numbers & dot
+  if ((key >= "0" && key <= "9") || key === ".") {
+    appendToDisplay(key);
+  }
+
+  // operators
+  else if (key === "+" || key === "-" || key === "*" || key === "/") {
+    appendToDisplay(key);
+  }
+
+  // enter = calculate
+  else if (key === "Enter") {
+    e.preventDefault(); // stop form submit
+    claculate();
+  }
+
+  // backspace
+  else if (key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  }
+
+  // clear
+  else if (key === "Escape") {
+    clearDisplay();
+  }
+});
